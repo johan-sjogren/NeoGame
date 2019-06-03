69b63b9b025192931bcfdb849ec3b564cdb9d5b2
Johan Sjögren <johan.sjogren@neodev.se>
2019-06-03 16:02:47 +0200

    Add a few tests to the greedy engine

--- AI/src/Agent/GreedyAgent.py
+++ AI/src/Agent/GreedyAgent.py
@@ -1,19 +1,19 @@
 import pandas as pd
 import numpy as np
 from . import BaseAgent
-
+from ..Engine import Game
 
 class GreedyAgent(BaseAgent.BaseAgent):
 
-    def __init__(self, value_func=None):
+    def __init__(self, value_func=Game.calc_score):
         self.value_func = value_func
 
     def get_action(self, state, actions, as_string=False):
         player_hand = np.array(state[:5])
-        opponent_table = state[-2:]
+        opponent_table = np.array(state[-2:])
 
-        possibles = [player_hand[a == 1] for a in actions]
-
+        # possibles = [player_hand[a == 1] for a in actions]
+        
         def diff_score(player, opponent):
             return (self.value_func(player, opponent) -
                     self.value_func(opponent, player))
@@ -24,6 +24,7 @@ class GreedyAgent(BaseAgent.BaseAgent):
                                 player_hand[a == 1], opponent_table), a),
                             actions))
 
+        # print(sorted(greedy_list, key=lambda x: x[0])[::-1])
         _, action = max(greedy_list, key=lambda x: x[0])
 
         if as_string:
@@ -35,3 +36,35 @@ class GreedyAgent(BaseAgent.BaseAgent):
     def learn(self, state, action, reward, new_state=None):
         # Static strategy, just return self
         return self
+    
+
+def test():
+    print("Running 3 tests on the greedy agent")
+    game = Game()
+    actions = game.get_actions()
+    agent = GreedyAgent(value_func=Game.calc_score)
+    
+    state_1 = [1, 2, 3, 4, 4, 0, 0, 1, 3]
+    action_1 = agent.get_action(state_1, actions)
+
+    # There are three equally good solutions here...
+    assert (action_1 == [1,1,0,0,0] or 
+            action_1 == [1,0,1,0,0] or
+            action_1 == [0,1,1,0,0]
+            )
+
+    state_2 = [2, 3, 3, 4, 4, 0, 4, 1, 4]
+    action_2 = agent.get_action(state_2, actions)
+    assert action_2 == [0, 1, 1, 0, 0]
+
+    state_3 = [0, 2, 2, 4, 4, 1, 2, 0, 3]
+    action_3 = agent.get_action(state_3, actions)
+    assert action_3 == [0, 1, 1, 0, 0]
+    print("Greedy agent passed all tests")
+
+def main():
+    pass
+    
+if __name__ == "__main__":
+    main()
+
