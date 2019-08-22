8f519e2bcda0a6ec2122c6aa792c79cd2f16b90c
Johan Sjögren <johan.sjogren@gmail.com>
2019-08-22 21:48:44 +0200

    Clean up of the engine class. Added and cleaned commentaries and docstrings. Also removed the sorting of the states as this should be done by the agents. Some will not need to sort at all.

--- AI/pyneogame/Engine.py
+++ AI/pyneogame/Engine.py
@@ -1,35 +1,30 @@
 from random import shuffle
 from itertools import permutations
+from collections import deque
 import numpy as np
 
 
-# TODO: Use player class to store hand/table info?
-class Player(object):
-
-    def __init__(self, score=0, hand=None, table=None, played=None):
-        self.score = score
-        self.hand = hand
-        self.table = table
-        self.played = played  # Card(s) played this round i.e. chosen actions
-
-
-# TODO: Export as json
-# TODO: Export only as list and json
-# TODO: Generalize to multiplayer
 class Game(object):
 
-    def __init__(self, n_classes=5, card_per_class=5, episodes=5):
-        deque = []
+    def __init__(self,
+                 n_classes=5,
+                 card_per_class=5,
+                 episodes=5,
+                 memory_length=10000,
+                 cards_in_hand=5,
+                 cards_on_table=2,
+                 cards_to_play=2):
+
+        deck = []
         self.n_classes = n_classes
         for x in range(n_classes):
-            deque += [x] * card_per_class
-        self.deque = np.array(deque)
-        self.player_score = []
-        self.opponent_score = []
-        self.n_cards_in_hand = 5
-        self.n_cards_to_play = 2
-        self.n_cards_on_table = 2
-
+            deck += [x] * card_per_class
+        self.deck = np.array(deck)
+        self.player_score = deque(maxlen=memory_length)
+        self.opponent_score = deque(maxlen=memory_length)
+        self.n_cards_in_hand = cards_in_hand
+        self.n_cards_to_play = cards_to_play
+        self.n_cards_on_table = cards_on_table
         self.actions = None
         # Initial setup
         self.dealCards()
@@ -41,55 +36,55 @@ class Game(object):
                    for y in player2_final)
 
     @staticmethod
-    def wins(x, y, max=4, min=0):
+    def wins(x, y, max_cls=self.n_classes-1, min_cls=0):
         if x + 1 == y:
             return 1
-        elif x == max and y == min:
+        elif x == max_cls and y == min_cls:
             return 1
         else:
             return 0
 
     def shuffleDeck(self):
-        shuffle(self.deque)
+        shuffle(self.deck)
         return self
 
     def dealCards(self):
-        """Randomly deal cards
-        Returns:
-            self
-        """
+        """Randomly deal cards"""
         self.shuffleDeck()
         i = self.n_cards_on_table
-        self.opponent_table = np.sort(self.deque[:i])
+        self.opponent_table = self.deck[:i]
         j, i = i, i + self.n_cards_in_hand
-        self.opponent_hand = np.sort(self.deque[j:i])
+        self.opponent_hand = self.deck[j:i]
         j, i = i, i + self.n_cards_on_table
-        self.player_table = np.sort(self.deque[j:i])
+        self.player_table = self.deck[j:i]
         j, i = i, i + self.n_cards_in_hand
-        self.player_hand = np.sort(self.deque[j:i])
+        self.player_hand = self.deck[j:i]
         return self
 
     def dealFromRecommendation(self, state):
-        """
-        Deals cards according to agent recommendation
+        """ Deals cards according to agent recommendation.
+        Arguments:
+            state {[int]} -- Integer list describing the recommended state
+
         Returns:
-            self
+            Game -- self
         """
+        # First recreate the recommended state and assign to Game variables
         i = self.n_cards_in_hand
-        # TODO: Should the engine really sort the cards? Maybe leave that to the Agents
-        # as some agents (e.g. DQNs) should be insensitive to the order
-        self.player_hand = np.sort(state[:i])
+        self.player_hand = state[:i]
         j, i = i, i + self.n_cards_on_table
-        self.player_table = np.sort(state[j:i])
+        self.player_table = state[j:i]
         j, i = i, i + self.n_cards_on_table
-        self.opponent_table = np.sort(state[j:i])
+        self.opponent_table = state[j:i]
         j, i = i, i + self.n_cards_in_hand
 
-        temp_deque = list(self.deque)
-        [temp_deque.remove(x) for x in state if x in temp_deque]
-        shuffle(temp_deque)
-        self.opponent_hand = np.sort(np.array(temp_deque[j:i]))
-
+        # Use a temporary deck
+        temp_deck = list(self.deck)
+        # Remove all cards already used in the recommended state
+        [temp_deck.remove(x) for x in state if x in temp_deck]
+        # The randomly pick opponents hand
+        shuffle(temp_deck)
+        self.opponent_hand = np.array(temp_deck[j:i])
         return self
 
     def getEnv(self):
@@ -98,17 +93,21 @@ class Game(object):
 
     def get_actions(self):
         '''
-        The actions are possible ways that you can pick n_cards_to_play cards
-        from hand. In this case represented by a boolean array for cards chosen
+        The actions are all possible ways that you can pick n_cards_to_play
+        cards from hand. In this case represented by a boolean array for
+        cards chosen
         '''
 
         if self.actions is None:
             n_ones = self.n_cards_to_play
             n_zeros = self.n_cards_in_hand-self.n_cards_to_play
-            self.actions = np.array(list(set(
-                            permutations(n_ones*[1]+n_zeros * [0],
-                                         self.n_cards_in_hand))))
-
+            # Generate all permutations
+            perms = permutations(n_ones*[1]+n_zeros * [0],
+                                 self.n_cards_in_hand)
+            # Remove duplicates
+            perms = set(perms)
+            # Convert to array
+            self.actions = np.array(list(perms))
         return self.actions
 
     def get_player_state(self):
@@ -124,12 +123,10 @@ class Game(object):
         return state
 
     def set_player_action(self, action):
-        # TODO: Perhaps this should change the table and hand list
         self.player_action = action.astype(bool)
         return self
 
     def set_opponent_action(self, action):
-        # TODO: Perhaps this should change the table and hand list
         self.opponent_action = action.astype(bool)
         return self
 
@@ -158,46 +155,57 @@ class Game(object):
     def get_scores(self):
         return self.score_player(), self.score_opponent()
 
-    # TODO: Move to test file
-    def test_player(self, player):
-        """ Routine to make sure that everyone plays nicely together """
+    def test_player(self, agent):
+        """Routine to test agent behaviour
+        Arguments:
+            agent {BaseAgent} -- Agent class inheriting from BaseAgent
+        """
+
         self.dealCards()
-        player_action = player.get_action(self.get_player_state(),
-                                          actions=self.get_actions())
-        
-        # Check that action is an numpy array
-        assert isinstance(player_action, np.ndarray)
+        agent_action = agent.get_action(self.get_player_state(),
+                                        actions=self.get_actions())
+
+        # Check that action recieved is a numpy array
+        assert isinstance(agent_action, np.ndarray)
 
         # Check that the player action produces the required number of cards
+        # Also checks that action can be convertet to boolean array
         try:
-            assert len(self.player_hand[player_action.astype(bool)]) == self.n_cards_to_play
+            assert len(self.player_hand[agent_action.astype(
+                bool)]) == self.n_cards_to_play
         except:
             print('Test failed for ', str(player))
             print('Expected number of cards:', self.n_cards_to_play)
-            print('Got: ', len(self.player_hand[player_action]))
+            print('Got: ', len(self.player_hand[agent_action]))
 
-# TODO: Move to testfile
+
 def test():
+    """
+    Checking basic functionality of the Game class
+    """
     game = Game()
     print('Running Engine tests')
-    # TODO: put in real test file
+
+    # Make sure that dealCards function actually randomizes
     for _ in range(10):
         setup_1, setup_2 = game.getEnv(), game.dealCards().getEnv()
         com_arrs = [np.array_equal(a, b) for x, y in zip(
             setup_1, setup_2) for a, b in zip(x, y)]
         # There is a chance that arrays will be similar just by chance
         assert sum(com_arrs) < 3
-    # Test the scoring function
+    # Test that the scoring function returns expected values
     assert game.calc_score([1, 1, 1, 1, 1], [2, 3, 4, 0, 0]) == 5
     assert game.calc_score([2, 3, 4, 0, 0], [1, 1, 1, 1, 1]) == 10
     assert Game.calc_score([2, 3, 4, 0, 0], [1, 1, 1, 1, 1]) == 10
     assert Game.calc_score([1, 1, 1, 1, 1], [2, 3, 4, 0, 0]) == 5
 
+    # Check that scoring never exceeds the highest possible score
     for _ in range(500):
-        arr1 = np.random.randint(0,5,4)
-        arr2 = np.random.randint(0,5,4)
+        arr1 = np.random.randint(0, 5, 4)
+        arr2 = np.random.randint(0, 5, 4)
         try:
-            assert Game.calc_score(arr1, arr2)<4*4
+            max_score = (game.n_cards_on_table + game.n_cards_to_play)**2
+            assert Game.calc_score(arr1, arr2) <= max_score
         except AssertionError:
             print(arr1)
             print(arr2)
@@ -211,6 +219,7 @@ def test():
         assert Game.calc_score(arr1, arr2) == 7
         assert Game.calc_score(arr2, arr1) == 6
 
+    # The list of actions should be equal for all game instances
     for _ in range(10):
         game1 = Game()
         game2 = Game()
@@ -221,6 +230,7 @@ def test():
 
 def main():
     test()
+
 
 if __name__ == "__main__":
     main()
