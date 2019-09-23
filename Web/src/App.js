1489b9b3f23f9d0a87d042fd60b926f9bfd9ccc3
unknown <anif@innab.se>
2019-09-23 15:05:15 +0200

    Added gamecontroller to app.js and include axios in package.json

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import GameController from "./components/GameController";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GameController />
+    </div>
+  );
 }
 
 export default App;
