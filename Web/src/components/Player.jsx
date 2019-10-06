afaa2300c4ef9e57d4aa21710150ee01aa3f2ed4
Therése Barmer <therese.barmer@gmail.com>
2019-10-06 12:44:55 +0200

    Add card component
    Solves: Jira NEOGAME-3
    Solves: Jira NEOGAME-8

--- Web/src/components/Player.jsx
+++ Web/src/components/Player.jsx
@@ -1,38 +1,23 @@
 import React from "react";
 import styles from "./player.module.css";
-function Player(props) {
-  const renderCards = () => {
-    let cards = props.hand.map((card, idx) => {
-      if (props.picks.includes(idx)) {
-        return (
-          <div
-            key={idx}
-            className={styles.pickedCard + " " + styles.card}
-            onClick={() => {
-              props.unpickCard(card, idx);
-            }}
-          >
-            {card}
-          </div>
-        );
-      } else {
-        return (
-          <div
-            key={idx}
-            className={styles.frontCard + " " + styles.card}
-            onClick={() => {
-              props.pickCard(card, idx);
-            }}
-          >
-            {card}
-          </div>
-        );
-      }
-    });
-    return cards;
-  };
 
-  return <div className={styles.playerContainer}>{renderCards()}</div>;
+import Card from './Card';
+function Player(props) {
+
+return(
+  <div className={styles.playerContainer}>
+    {props.hand.map((id, idx) =>
+      <Card
+        key={idx}
+        cardId={id}
+        idx={idx}
+        pickCard={props.pickCard}
+        unpickCard={props.unpickCard}
+        picked = {props.picks.includes(idx)}
+      />
+    )}
+  </div>
+  );
 }
 
 export default Player;
