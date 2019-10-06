044239a712baf85d62fc77324afec86b12b0d782
David Rådberg <david.radberg@neodev.se>
2019-10-06 10:56:45 +0200

    Working on DeepQAgent
    Creating a nn model that works
    for the DeepQAgent. Also adjusting
    the training script to autosave
    and test every episode

--- AI/training_script.py
+++ AI/training_script.py
@@ -4,31 +4,33 @@
 from pyneogame.gym import Gym
 from pyneogame.Agent.QTableAgent import QTableAgent
 from pyneogame.Agent.GreedyAgent import GreedyAgent
-from pyneogame.Agent.PolicyGradient import ReInforce
 from pyneogame.Agent.DeepQAgent import DeepQAgent
 from pyneogame.Engine import Game
-# Start the game
-game = Game()
+
+FILE_NAME = "dq_agent"
+TRAINING_ITERATIONS = 100000
+NUM_EPISODES = 5
+UPDATE_INTERVAL = int(TRAINING_ITERATIONS / NUM_EPISODES)
+TEST_ITERATIONS = 10000
 
 # a) Setup the agents
-player = ReInforce(state_size=len(game.get_player_state()),
-                   actions=game.get_actions())
-player.load('models/pg_best.h5',
-            custom_objects={'reward_loss': player.reward_loss})
-# player = DeepQAgent(state_size=len(game.get_player_state()),
-                #    actions=game.get_actions())
+game = Game()
+player = DeepQAgent(state_size=len(game.get_player_state()),
+                       actions=game.get_actions(), update_interval=UPDATE_INTERVAL, filename=FILE_NAME)
 opponent = GreedyAgent()
 
 # b) Setup the gym
 gym = Gym(player, opponent, game)
 
 # c) Run training
-gym.train(num_episodes=2000)
-gym.eval_exp_table()
+for i in range(NUM_EPISODES):
+    gym.train(num_episodes=UPDATE_INTERVAL)
+    gym.eval_exp_table()
+    gym.test(int(TEST_ITERATIONS / NUM_EPISODES))
+    gym.eval()
 
 # d) Run test
-gym.test()
+gym.test(TEST_ITERATIONS)
 gym.eval()
 
-player.save('models/pg_best.h5')
-# player.save('models/dq_best.h5')
+player.save(FILE_NAME + ".h5")
