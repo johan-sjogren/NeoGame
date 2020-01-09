80bb722b47cd6a379b7abef68ac3fb9c072a379a
Rita Hermodsson <ritahermodsson@gmail.com>
2020-01-09 13:49:35 +0100

    NEOGAME-50 style play button

--- Web/src/components/Score.jsx
+++ Web/src/components/Score.jsx
@@ -3,12 +3,12 @@ import styles from "./score.module.css";
 function Score(props) {
   return props.player ? (
     <>
-      {"Player"}
+      <div style={{ color: "white" }}>Player</div>
       <div className={styles.board + " " + styles.player}>{props.score}</div>
     </>
   ) : (
     <>
-      {"Opponent"}
+      <div style={{ color: "white" }}>Opponent</div>
       <div className={styles.board + " " + styles.opponent}>{props.score}</div>
     </>
   );
