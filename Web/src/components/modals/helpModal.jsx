8af67730e9f9057352765ebb43243f24151600c8
Rita Hermodsson <ritahermodsson@gmail.com>
2020-01-10 09:25:57 +0100

    added help text

--- Web/src/components/modals/helpModal.jsx
+++ Web/src/components/modals/helpModal.jsx
@@ -17,7 +17,11 @@ function HelpModal(props) {
             {"The game"}
           </Modal.Title>
         </Modal.Header>
-        <Modal.Body>{"Write text here"}</Modal.Body>
+        <Modal.Body>
+          {
+            "NeoGame is a 2 player card game, where one player takes the role of the development team and the other will play as the dreaded IT-project. The deck is shuffled for each round, then each player receives two cards lying face-up and a hand of five cards. The players selects two cards from their hand and places them on the table. The players score is calculated as follows: For each card, points are rewarded for everyone of the opponent's card that it beats.  Cards beat each other according to a circle chain where level one cards (Bugg/Product owner) beats level two cards (Scrummaster/Specifications) and so on."
+          }
+        </Modal.Body>
       </Modal>
     </>
   );
