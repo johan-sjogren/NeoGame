e0c1f10515427a4375339d7ff748dff962c9c6d1
dstaahl <51971686+dstaahl@users.noreply.github.com>
2020-01-16 16:01:02 +0100

    AI/scripts: Adding shell script option to run jobs using submitted packages (#25)

--- AI/scripts/gcp.sh
+++ AI/scripts/gcp.sh
@@ -5,13 +5,20 @@
 # GCP settings
 BUCKET=gs://<YOUR-BUCKET>
 REGION=<YOUR-REGION>
+SCALE_TIER=BASIC
+RUNTIME_VERSION=1.14
+PYTHON_VERSION=3.5
 
-# Task settings
+# Local settings
 PARENT=$(dirname "$0")/..
+TAR_NAME=NeoGameAI-0.1.0.tar.gz
 PACKAGE=trainer
 PACKAGE_PATH=$PARENT/$PACKAGE
-PACKAGE_TAR=$PARENT/dist/NeoGameAI-0.1.0.tar.gz
+PACKAGE_TAR=$PARENT/dist/$TAR_NAME
 TASK=task
+
+# Host settings
+PACKAGE_TAR_HOST=$BUCKET/$TAR_NAME
 
 submit_package()
 {
@@ -20,17 +27,31 @@ submit_package()
   gsutil cp $PACKAGE_TAR $BUCKET
 }
 
+run_package_job()
+{
+  JOBNAME=${TASK}_$(date -u +%Y%m%d_%H%M%S)
+  gcloud ai-platform jobs submit training $JOBNAME \
+    --packages $PACKAGE_TAR_HOST \
+    --module-name $PACKAGE.$TASK \
+    --region $REGION \
+    --scale-tier $SCALE_TIER \
+    --runtime-version $RUNTIME_VERSION \
+    --python-version $PYTHON_VERSION \
+    -- \
+    -o $BUCKET/$JOBNAME
+}
+
 submit_job()
 {
   JOBNAME=${TASK}_$(date -u +%Y%m%d_%H%M%S)
   gcloud ai-platform jobs submit training $JOBNAME \
-    --package-path=$PACKAGE_PATH \
-    --module-name=$PACKAGE.$TASK \
-    --region=$REGION \
-    --staging-bucket=$BUCKET \
-    --scale-tier=BASIC \
-    --runtime-version=1.14 \
-    --python-version 3.5 \
+    --package-path $PACKAGE_PATH \
+    --module-name $PACKAGE.$TASK \
+    --region $REGION \
+    --staging-bucket $BUCKET \
+    --scale-tier $SCALE_TIER \
+    --runtime-version $RUNTIME_VERSION \
+    --python-version $PYTHON_VERSION \
     -- \
     -o $BUCKET/$JOBNAME
 }
@@ -39,11 +60,18 @@ case $1 in
   submit_package)
     submit_package
     ;;
+  run_package_job)
+    run_package_job
+    ;;
   submit_job)
     submit_job
     ;;
   *)
-    echo Please 'submit_job'
+    echo "$0"
+    echo "Options:"
+    echo "    submit_package"
+    echo "    run_package_job"
+    echo "    submit_job"
     ;;
 esac
   
