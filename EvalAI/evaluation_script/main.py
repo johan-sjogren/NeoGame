b55edf9eacf24df33b7daf1cdc720a85a3fd5246
Johan Sjögren <johan.sjogren@neodev.se>
2021-03-15 09:56:44 +0100

    Updated and tested a new zip-file based submission evaluation

--- EvalAI/evaluation_script/main.py
+++ EvalAI/evaluation_script/main.py
@@ -1,44 +1,78 @@
 import random
-from .pyneogame.gym import Gym
-from .pyneogame.Agent.GreedyAgent import GreedyAgent
-from .pyneogame.Agent.DeepQAgent import DeepQAgent
-from .pyneogame.Agent.RandomAgent import RandomAgent
-from .pyneogame.Engine import Game
+import os
+import zipfile
+from pyneogame.gym import Gym
+from pyneogame.Agent.GreedyAgent import GreedyAgent
+from pyneogame.Agent.DeepQAgent import DeepQAgent
+from pyneogame.Agent.RandomAgent import RandomAgent
+from pyneogame.Engine import Game
 from pathlib import Path
 
-# This will be run by the eval worker
 
-def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwargs):
+class SimulationError(Exception):
+    """Exception raised for incorrect submission files."""
+
+    def __init__(self):
+        self.message = ("Submitted Zipfile doesn't contain expected files")
+        super().__init__(self.message)
+
+
+def evaluate(test_annotation_file, user_submission_file,
+             phase_codename, **kwargs):
+    # This will be run by the eval worker
     print("Starting Evaluation.....")
 
     output = {}
     output["result"] = [
-    {
-        "test_split": {
+        {
+            "test_split": {}
         }
-    }
     ]
 
-    N_GAMES = 50000
-    DQ_FILE = str(Path(__file__).parent.absolute())+'/models/dq_agent.h5'
+    N_GAMES = 50
+    DQ_FILE = str(Path(__file__).parent.absolute())+'/models/dq_agent.h5'    
+    game = Game()
 
     print("Evaluating model: {}".format(user_submission_file))
+    print('Extracting from zipfile')
+    files_to_remove = []
+    zipf = zipfile.ZipFile(user_submission_file)
+    agentfile = [x for x in zipf.namelist() if 'CustomAgent.py' in x]
+    h5file = [x for x in zipf.namelist() if 'CustomAgent.h5' in x]
+    if len(agentfile) == 1:
+        agentfile = agentfile[0]
+        files_to_remove.append(zipf.extract(agentfile))
+    else:
+        raise BadSubmissionError()
 
-    game = Game()
-    player = DeepQAgent(state_size=len(game.get_player_state()),
-                           actions=game.get_actions())
-    player.load(user_submission_file)
+    if len(h5file) == 1:
+        h5file = h5file[0]
+        files_to_remove.append(zipf.extract(h5file))
+    else:
+        h5file = None
+
+    print('Instantiating submitted agent')
+    from CustomAgent import CustomAgent
+    if h5file is not None:
+        player = CustomAgent.from_game_and_h5(game, h5file)
+    else:
+        player = CustomAgent.from_game(game)
+
+    print('Instantiate opponents')
+    agent_dict = {}
     random_agent = RandomAgent()
+    agent_dict['Random'] = random_agent
     greedy_agent = GreedyAgent()
-    dq_agent = DeepQAgent(state_size=len(game.get_player_state()),
-                         actions=game.get_actions()).load(DQ_FILE)
+    agent_dict['Greedy'] = greedy_agent
+    try:
+        dq_agent = DeepQAgent(state_size=len(game.get_player_state()),
+                              actions=game.get_actions()).load(DQ_FILE)
+        agent_dict['DeepQ'] = dq_agent
+    except IOError:
+        print('DQ file not found. Excluded from test')
 
-    agent_dict = {'Random': random_agent,
-              'Greedy': greedy_agent,
-              'DeepQ': dq_agent
-    }
+    print('Running test cycle')
     total_score = 0
-
     for agent_name in agent_dict:
         gym = Gym(player, agent_dict[agent_name])
         gym.test(N_GAMES)
@@ -52,4 +86,9 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
 
     print(output)
     print("Evaluation Done!")
+    print("Clean up")
+
+    for filename in files_to_remove:
+        if os.path.isfile(filename):
+            os.remove(filename)
     return output
